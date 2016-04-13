18579509b6d4fad487f6ff6feb22713026c2fd64
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2016-04-13 13:57:38 +0200

    leny : fix pos

--- js/wearegifanimals.js
+++ js/wearegifanimals.js
@@ -51,7 +51,7 @@ var maxBackground = 0;                          // Size of the backgroundBank in
 
 var animemalBank = [
 <!--WAGA_NEW_ANIMEMAL-->
-                    {name:"leny_morue", zindex:100, bottom:"20%", top:"auto", right:"auto", left:"35%", width: "90px", height:"auto" },
+                    {name:"leny_morue", zindex:100, bottom:"20%", top:"auto", right:"auto", left:"35%", width: "100px", height:"auto" },
                     {name:"aminata_mantris", zindex:100, bottom:"auto", top:"20%", right:"auto", left:"36%", width: "90px", height:"auto" },
                     {name:"milo_calamar", zindex:100, bottom:"auto", top:"33%", right:"18%", left:"auto", width: "90px", height:"auto" },
                     {name:"anais_CHOUETTE", zindex:100, bottom:"auto", top:"45%", right:"auto", left:"25%", width: "90px", height:"auto" },
